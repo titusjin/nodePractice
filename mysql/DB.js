bcf0115bc30ff4e53a181336020aee9c71041025
smgdevelop01 <titus.jin@stormmediagroup.com>
2016-11-03 17:57:54 +0800

    updated

---
+++ mysql/DB.js
@@ -0,0 +1,31 @@
+'use strict';
+const mysql2nativePromise = require('mysql2/promise');
+const async = require('asyncawait/async');
+const await = require('asyncawait/await');
+
+
+const cfOption = {
+    "host": "localhost",
+    "user": "root",
+    "password": "root",
+    "database": "mysql"
+};
+
+const conn = mysql2nativePromise.createConnection(cfOption);
+
+var DB = function(sql, params) {
+    return conn.then(function(connection){
+        return connection.execute(sql, params);
+    });
+};
+
+DB.one = async(function(sql, params){
+    var result = await(DB(sql, params)
+        .then(rows => {
+            return rows[0];
+        }));
+    return result;
+    // console.log(result);
+});
+
+module.exports = DB;
--- mysql/mysqlTest.js
+++ mysql/mysqlTest.js
@@ -1,7 +1,9 @@
 'use strict';
 
-var appRoot = require('app-root-path');
-var db      = require('mysql-promise')();
+var mysql = require('mysql2');
+var mysql2nativePromise = require('mysql2/promise');
+
+var db = require('mysql-promise')();
 
 var cfOption = {
     "host": "localhost",
@@ -10,10 +12,58 @@ var cfOption = {
     "database": "mysql"
 };
 
-db.configure(cfOption);
-
-db.query('SELECT user,password_expired FROM USER').spread((users) => {
-    console.log('hello ' + users);
-
-    process.exit(0);
+/*
+  mysql2 direct usage
+ */
+var connection = mysql.createConnection({host: 'localhost' , user: 'root', password: 'root', database:'mysql' });
+connection.execute('SELECT user FROM USER WHERE `user` = ?', [`root`], function (err, results, fields) {
+  console.log(results[0]); // results contains rows returned by server
+  // process.exit(0);
 });
+
+
+/*
+  mysql2 native-promise usage
+ */
+mysql2nativePromise.createConnection({host: 'localhost' , user: 'root', password: 'root', database:'mysql' })
+    .then(function(conn){
+        return conn.execute('SHOW databases');
+    })
+    .then(function(rows){
+        console.log(rows);
+        return rows;
+    });
+
+
+/*
+ mysql2 native async usage
+ */
+// async function testNativeAsync() {
+//     var conn2 = await mysql2nativePromise.createConnection({host: 'localhost' , user: 'root', password: 'root', database:'mysql' });
+//     var result = await conn2.query('show databases');
+//     console.log(result);
+// }
+//
+// testNativeAsync();
+
+// console.log(rows);
+    // .then((conn) => {
+    //     conn.query('SELECT user FROM USER');
+    // })
+    // .then((rows, fields) => {
+    //     console.log(fields);
+    //     process.exit(0);
+    // });
+
+
+/*
+  mysql-promise npm module usage
+ */
+db.configure(cfOption);
+db.query('SELECT user,password_expired FROM USER WHERE user=? ', 'root')
+    .then((rows) => {
+        console.log(rows[0]);
+    });
+
+
+// rows.sayhello = () => 'hello !! ';
