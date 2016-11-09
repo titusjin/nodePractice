31d59d125f6c6e2bfabe32c0e9b37d7dfcdc4315
titus jin <titusjin@gmail.com>
2016-11-09 14:03:02 +0800

    adding inde.js in titus folder and require it in entry.js without specific filename cause index can be searched automatically.

--- entry.js
+++ entry.js
@@ -1,5 +1,6 @@
 var jin = require("./describe.js");
 require('./simplereq.js');
-
+var greet = require('./titus');
 
 console.log(jin.exposeName());
+greet();
---
+++ titus/index.js
@@ -0,0 +1,7 @@
+'use strict';
+
+function greet(){
+  console.log('HELLO deepblu!');
+}
+
+module.exports = greet;
