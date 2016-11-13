b748fbe56640b83095980655a93565319bb63281
titus jin <titusjin@gmail.com>
2016-11-13 13:07:09 +0800

    class declaration practice

---
+++ class/classtest.js
@@ -0,0 +1,9 @@
+'use strict';
+
+class Titus {
+  constructor(name){
+    this.name = name;
+  }
+}
+
+module.exports = Titus;
---
+++ class/classtestentry.js
@@ -0,0 +1,8 @@
+'use strict';
+
+const TitusClass = require('./classtest');
+console.log(typeof TitusClass);
+
+const titus = new TitusClass('deepblu');
+
+console.log(titus.name);
