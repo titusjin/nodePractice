fad357410fb3fcf804874c10a913665cea88ca65
titus jin <titusjin@gmail.com>
2017-10-01 22:22:25 +0800

    upadte the current version

--- promise/promiseTest.js
+++ promise/promiseTest.js
@@ -9,48 +9,51 @@ const API_KEY = "?key=PAPERCLIP1234";
 let doTest = () => {
     return axios.get(`${ROOT_URL}/posts${API_KEY}`)
                 .then( result => {
-                    console.log('IN axios then ', result);
+                    // console.log('IN axios then ', result);
                     return result;
                 })
                 .catch( err => {
-                    console.log('FAIL +++++++++');
+                    // console.log('FAIL +++++++++');
                 });
 }
-let doTest2 = () => {
+let doTest2 = (i) => {
     return axios.get(`${ROOT_URL}/p${API_KEY}`)
                 .then( result => {
-                    console.log('IN axios then ', result);
+                    // console.log('IN axios then ', result);
                     return result;
                 })
                 .catch( err => {
-                    console.log('IN axios catch ', err);
-                    return {name: 'AXIOS_FAIL', status: err.response.status};
+                    // console.log('IN axios catch ', err);
+                    return {name: `AXIOS_FAIL`, status: err.response.status};
                 });
 }
 
 
 let doStart = () => {
     let promiseArray = [];
-    for( let i = 0 ; i < 2 ; i++ ){
+    for( let i = 0 ; i < 3 ; i++ ){
         console.log(i);
-        if( i == 0 ){
-            promiseArray.push( doTest2() );
+
+        if( i == 1 ){
+            promiseArray.push( doTest2(i) );
         }else{
             promiseArray.push( doTest() );
         }
     }
 
-    console.log('the promiseArray length is : ', promiseArray.length);
-
     Promise.all( promiseArray )
         .then( results => {
             console.log('the return start ********');
 
+            let result = 0;
             for(let i = 0 , leng = results.length ; i < leng ; i++ ){
-                if(results[i].status !== 200){
-                    console.log(results[i]);
+
+                if( (results[i] && results[i].status !== 200) || !results[i] ) {
+                    result +=1;
                 }
             }
+
+            console.log('failied total : ', result);
             console.log('the return end ********');
         }, reject => {
             console.log('THE reject start ---------');
