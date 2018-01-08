e6625dd62a0f9041a7b632136440d23c0346a565
titus jin <titusjin@gmail.com>
2018-01-08 23:19:01 +0800

    adding latest process of revose_vowles.js

--- leeCode/revose_vowles.js
+++ leeCode/revose_vowles.js
@@ -7,6 +7,13 @@ const vowArray = [
 ]
 
 
+let f = (str) => {
+    let inputCharArray = str.toLowerCase().split('').sort();
+    const leng = inputCharArray.length;
+    for (let i = 0 ; i < leng ; i++){
+        
+    }
+}
 
 
 
